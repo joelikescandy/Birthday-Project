5160e23abf99829506e9d883da53ad3db38309a0
joelikescandy <dbaerLA@gmail.com>
2024-06-28 16:24:12 -0700

    Update script.js

--- script.js
+++ script.js
@@ -17,12 +17,18 @@ myImage.addEventListener("click", function () {
   responses[2] = "Maybe";
   responses[3] = "Ask again later";
   responses[4] = "Feed Me"
-  responses[5] = "What does that mean about you?"
+  responses[5] = "What does that say about you?"
   responses[6] = "Why should I care?"
   responses[7] = "*snores*"
-
-  
-  
+  responses[8] = "Don't be Emo"
+  responses[9] = "Do it" 
+  responses[10] = "You ate"
+  responses[11] = "Absolutely not"
+  responses[12] = "Go eat" 
+  responses[13] = "Chill out"
+  responses[14] = "Do it no balls"
+  responses[15] = "Go to sleep"
+  responses[16] = "Meow"
     let item = responses[Math.floor(Math.random()*responses.length)];
 
   while (true) { 
